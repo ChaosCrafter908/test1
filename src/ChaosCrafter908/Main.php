d608c34c5a0d3c30fcd01511a1014c3eff9b03b7
ChaosCrafter908 <68608362+ChaosCrafter908@users.noreply.github.com>
2020-07-27 15:34:34 +0200

    Update Main.php

--- src/ChaosCrafter908/Main.php
+++ src/ChaosCrafter908/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace ChaosCrafter908;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+
+public function onEnable() {
+
+this->getServer()->getPluginManager()->registerEvents(this, this);
+
+}
+public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){}
+}
